cfc868c837f17fb3215d134301c54eb7097d98d8
roiqd03 <roiquint@ucm.es>
2025-04-22 19:16:36 +0200

    gestion de memoria

--- TelemetrySystem/Src/Persistence.h
+++ TelemetrySystem/Src/Persistence.h
@@ -2,6 +2,7 @@
 #include "TrackerEvent.h"
 #include "CircularQueue.h"
 #include <queue>
+#include <unordered_set>
 
 class ISerializer;
 
@@ -15,11 +16,13 @@ public:
 	void SetSerializer(ISerializer* serializer);
 	ISerializer* GetSerializer();
 	virtual void Release() = 0;
+	void DeleteEvents();
 protected:
 	const std::string SuddenSerialization(); //Serializa de golpe todos los eventos
 	virtual void Flush() = 0;
 	circular_buffer<TrackerEvent*>* eventsQueue;
 	uint32_t numBeforeFlush = 100; // El número de eventos encolados antes de hacer el flush
 	ISerializer* _serializer = nullptr;
+	std::unordered_set<TrackerEvent*> _eventsToDelete;
 };
 
